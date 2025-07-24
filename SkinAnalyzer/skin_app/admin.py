c1463a3e69124b8be87d9d42511abfcb52e1aaf9
ananya <ananyajoshi0707@gmail.com>
2025-07-24 13:01:45 +0530

    Update: latest changes in Django project

--- SkinAnalyzer/skin_app/admin.py
+++ SkinAnalyzer/skin_app/admin.py
@@ -1,10 +1,20 @@
-from django.contrib import admin
+from django.contrib import admin 
 from .models import SkinAnalysis
 
+@admin.register(SkinAnalysis)
 class SkinAnalysisAdmin(admin.ModelAdmin):
-    list_display = ('id', 'recommendations', 'uploaded_at')
+    list_display = ('id', 'image_preview', 'condition_short', 'recommendations', 'uploaded_at')
+    readonly_fields = ('image_preview',)
+    search_fields = ('condition', 'recommendations')
+    list_filter = ('uploaded_at',)
 
-admin.site.register(SkinAnalysis,SkinAnalysisAdmin)
+    def image_preview(self, obj):
+        if obj.image:
+            return f'<img src="{obj.image.url}" width="100" />'
+        return "No Image"
+    image_preview.allow_tags = True
+    image_preview.short_description = "Image"
 
-
-# Register your models here.
+    def condition_short(self, obj):
+        return obj.condition[:50] if obj.condition else "Pending"
+    condition_short.short_description = "Condition"
